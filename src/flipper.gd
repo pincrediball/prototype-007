6a1c84b911507feabb27a015c0d88ad125fe5b93
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-03-06 23:15:22 +0100

    Finalize prototype

--- src/flipper.gd
+++ src/flipper.gd
@@ -3,20 +3,28 @@ extends Node2D
 @export
 var max_angle = 55
 
+@export
+var up_time = 0.05
+
+@export
+var isRunning = false
+
 var is_going_up = true
 var time_moving = 0.0
-const up_time = 0.05
-const down_time = 0.5
+const down_time = 0.3
 
 func _physics_process(delta):
+	if !isRunning:
+		return
+	
 	time_moving += delta
-
+	
 	if is_going_up:
 		$CharacterBody2D.rotation_degrees = max_angle * (time_moving / up_time)
 		if time_moving > up_time:
 			is_going_up = false
 			time_moving = 0.0
-
+	
 	else:
 		$CharacterBody2D.rotation_degrees = max_angle * (1 - (time_moving / down_time))
 		if time_moving > down_time:
--- src/tutorial_machine.gd
+++ src/tutorial_machine.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
-@onready
-var ball: RigidBody2D = get_node("Ball")
+var ballScene = preload("res://Ball.tscn")
+var ball: Node
 
-const ballStartX = 500
-const ballStartY = 435
+const ballStartX = 1022
+const ballStartY = 735
 
 func _on_run_playbook_button_button_up():
+	get_tree().create_timer(0.5).timeout.connect(startFlipperLeft)
+	get_tree().create_timer(0.35).timeout.connect(startFlipperRight)
+	var ball: RigidBody2D = ballScene.instantiate()
+	ball.position = Vector2(ballStartX, ballStartY)
 	ball.linear_velocity = Vector2(0, -2000)
+	$BallsGroup.add_child(ball)
+	
+func startFlipperLeft():
+	$FlipperLeft.isRunning = true
+	
+func startFlipperRight():
+	$FlipperRight.isRunning = true
 
 func _on_drainage_body_entered(body):
 	if body == ball:
