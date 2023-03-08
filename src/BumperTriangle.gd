8b9c7ed37ea0985927c48b3ddc98e0b7654147db
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-03-08 18:11:18 +0100

    Finish up Prototype 007 work for the day

--- src/BumperTriangle.gd
+++ src/BumperTriangle.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 
 func on_ball_exit(ball: RigidBody2D):
 	if $SlingArea2D.overlaps_body(ball):
-		var direction = (ball.position - self.position).normalized()
-		var impulse = direction * 1000
+		var direction = $SlingArea2D/CollisionShape2D.global_rotation
+		var baseForce = Vector2(1, 0).rotated(direction)
+		var impulse = baseForce * 1000
 		ball.apply_impulse(impulse)
